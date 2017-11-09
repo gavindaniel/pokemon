13504e3e3c725bfdc219d19505af8222285d61cd
asaad <asaad@email.arizona.edu>
2017-11-08 18:06:46 -0700

    Updated Trainer with battle instance variable

--- src/model/Trainer.java
+++ src/model/Trainer.java
@@ -8,9 +8,11 @@ import pokemon.Pokemon;
 public class Trainer {
 
 	private String name;
-	private List<Pokemon> pokemonList;
+	private List<Pokemon> ownedPokemonList;		//List of all owned pokemon
+	private List<Pokemon> battlePokemonList;		//List of 3 pokemon to be used in battle
+	private Pokemon activeBattlePokemon;			//Active pokemon in battle
 //	private List<Item> itemList;
-	private Point position;
+	private Point position;		//Position on map
 
 //	private TrainerAnimation trainerAnimation;
 
@@ -33,17 +35,46 @@ public class Trainer {
 	}
 
 	/**
-	 * @return the pokemonList
+	 * @return the ownedPokemonList
 	 */
-	public List<Pokemon> getPokemonList() {
-		return pokemonList;
+	public List<Pokemon> getOwnedPokemonList() {
+		return this.ownedPokemonList;
 	}
 
 	/**
 	 * @param pokemonList the pokemonList to set
 	 */
 	public void setPokemonList(List<Pokemon> pokemonList) {
-		this.pokemonList = pokemonList;
+		this.ownedPokemonList = pokemonList;
+	}
+	
+
+	/**
+	 * @return the battlePokemonList
+	 */
+	public List<Pokemon> getBattlePokemonList() {
+		return battlePokemonList;
+	}
+
+	/**
+	 * @param battlePokemonList the battlePokemonList to set
+	 */
+	public void setBattlePokemonList(List<Pokemon> battlePokemonList) {
+		this.battlePokemonList = battlePokemonList;
+	}
+
+	/**
+	 * @return the activeBattlePokemon
+	 */
+	public Pokemon getActiveBattlePokemon() {
+		return activeBattlePokemon;
+	}
+
+	/**
+	 * @param activeBattlePokemon the activeBattlePokemon to set
+	 */
+	public void setActiveBattlePokemon(Pokemon activeBattlePokemon) {
+		this.activeBattlePokemon = activeBattlePokemon;
 	}
 
 	/**
@@ -63,13 +94,22 @@ public class Trainer {
 //	public void move() {
 //
 //	}
-
-	public boolean addPokemon(Pokemon p) {
-		return this.pokemonList.add(p);
+	/**
+	 * Append pokemon to owned list.
+	 * @param p pokemon to add
+	 * @return true if successful
+	 */
+	public boolean addPokemonToOwned(Pokemon p) {
+		return this.ownedPokemonList.add(p);
 	}
 
-	public boolean removePokemon(Pokemon p) {
-		return this.pokemonList.remove(p);
+	/**
+	 * Delete pokemon from owned list
+	 * @param p pokemon to delete
+	 * @return ture if successful
+	 */
+	public boolean removePokemonFromOwned(Pokemon p) {
+		return this.ownedPokemonList.remove(p);
 	}
 
 //	public boolean addItem() {
