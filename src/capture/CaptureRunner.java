5edb5a0a6681448abcec235431eaec51efdce36f
ianohurr <ianoheir@email.arizona.edu>
2017-11-22 02:20:04 -0700

    capture view done, but missing animations for other pokemon

---
+++ src/capture/CaptureRunner.java
@@ -0,0 +1,73 @@
+package capture;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
+
+import battle.BattleLogic;
+import battle.BattleView;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import model.Trainer;
+import pokemon.Bulbasaur;
+import pokemon.Charmander;
+import pokemon.Pikachu;
+import pokemon.Pokemon;
+import pokemon.Squirtle;
+
+public class CaptureRunner extends Application {
+
+	private Capture capture;
+	private Trainer trainer1;
+	private Trainer trainer2;
+	private Observer captureView;
+
+	private BorderPane window;
+
+	public static final double SCENE_WIDTH = 800;
+	public static final double SCENE_HEIGHT = 550;
+	
+	private static final Image bgImg = new Image("file:images/battle/battle-background.png", false);
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		//Initialize main border pane and scene
+		stage.setTitle("Pokemon Capture");
+		window = new BorderPane();
+		Scene scene = new Scene(window, SCENE_WIDTH, SCENE_HEIGHT);
+		//Add battle view observer
+		trainer1=new Trainer("Person");
+		capture=new Capture(new Pikachu(), trainer1);
+		captureView = new CaptureView2(capture, SCENE_WIDTH, SCENE_HEIGHT,window);
+		window.setCenter((Node) captureView);
+		stage.setScene(scene);
+		stage.show();
+		
+		
+	}
+	
+	
+	
+	
+		   
+		   
+		   
+
+	   
+	 }
+
---
+++ src/capture/CaptureView2.java
@@ -0,0 +1,151 @@
+package capture;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Random;
+
+import items.Bait;
+import items.Rock;
+import items.SafariBall;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+public class CaptureView2 extends Canvas implements Observer {
+	
+	private Capture capture;
+	private GraphicsContext gc;
+	private Image battleGround;
+	private BorderPane window;
+	Button throwBallButton;
+	 Button throwBaitButton;
+	 Button throwRockButton;
+	 PokeCaptureAnimation captureAnimation;
+	
+	
+	public CaptureView2(Capture capture, double width, double height,BorderPane window) {
+		
+		this.setWidth(width);
+		this.setHeight(height);
+		this.window=window;
+		this.capture = capture;
+		captureAnimation=new PokeCaptureAnimation(this,"file:images/battle/battle-background.png",capture.currentPoke.getStandByPath(),capture.currentPoke.getRunAwayPath(),capture.currentPoke.getCapturePath());
+		gc = this.getGraphicsContext2D();
+		
+		battleGround = new Image("file:images/battle/battle-background.png", false);
+		
+		gc.drawImage(battleGround, 0, 0, this.getWidth(), this.getHeight());
+		setUpCapture();
+		setUpCaptureButtons();
+		
+
+		
+	}
+	
+	
+	public void setUpCapture() {
+		
+		captureAnimation.animateStandby();
+	}
+	
+	
+	@Override
+	/**
+	 * Called by the setChanged/notifyObserver methods in the observable.
+	 */
+	public void update(Observable arg0, Object arg1) {
+		printBattleStage();
+	}
+	
+	private void printBattleStage() {
+		
+		//Draw Background
+		gc.drawImage(battleGround, 0, 0, this.getWidth(), this.getHeight());
+	}
+	
+	
+	
+	
+	
+	// This event handler will handle the buttons for catching the Pokemon 
+	  private class CaptureButtonListener implements EventHandler<ActionEvent> {
+	    @Override
+	    public void handle(ActionEvent event) {
+	    	if(captureAnimation.canUseItem()) {
+	    	if(event.getSource()==throwBallButton) {
+	    		if(capture.checkHasSafariBalls()) {
+	    		captureAnimation.animateItemThrow(new SafariBall());
+	    		capture.currentTrain.removeSafariBall();
+	    		}
+	    		else {
+	    			System.out.println("Out of pokeballs");
+	    		}
+	    }
+	    	if(event.getSource()==throwBaitButton) {
+	    		captureAnimation.animateItemThrow(new Bait());
+	    		capture.throwBait();
+	    }
+	    	if(event.getSource()==throwRockButton) {
+	    		captureAnimation.animateItemThrow(new Rock());
+	    		capture.throwRock();
+	    }
+	  }
+	    	
+	    }
+	  }
+	  
+	  //Let the pokemon try to retreat
+	  public void attemptRetreat() {
+		  
+	    	if(capture.retreat(new Random().nextInt(100))) {
+	    		captureAnimation.animateRunAway();
+	    	}
+	    	
+	  }
+	  
+	  
+	  //Try to capture the pokemon
+	  public void attemptCapture() {
+		  
+		  if(capture.throwBall(new Random().nextInt(100))) {
+			  System.out.println(capture.currentTrain.getOwnedPokemonList());
+			  captureAnimation.animateCapture();
+		  }
+			  
+		  
+	  }
+	  
+	  
+	  
+	  
+	  
+	  
+	//Set up capture buttons
+	 public void setUpCaptureButtons() {
+		 GridPane captureButtons=new GridPane();
+		 throwBallButton = new Button("Throw Ball");
+		 throwBaitButton = new Button("Throw Bait");
+		 throwRockButton = new Button("Throw Rock");
+		 captureButtons.add(throwBallButton, 0, 0);
+		 captureButtons.add(throwBaitButton, 1, 0);
+		 captureButtons.add(throwRockButton, 2, 0);
+		 captureButtons.setAlignment(Pos.CENTER);
+		 window.setTop(captureButtons);
+		 BorderPane.setAlignment(captureButtons, Pos.BOTTOM_CENTER);
+		 throwBallButton.setOnAction(new CaptureButtonListener());
+		 throwBaitButton.setOnAction(new CaptureButtonListener());
+		 throwRockButton.setOnAction(new CaptureButtonListener());
+	 }
+	
+	
+	
+	
+	
+
+}
